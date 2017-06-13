436679c2df5ff45b5094316c42b6025d951c308c
Edmund Do <edo@uvm.edu>
2017-06-13 17:13:38 -0400

    Added resizable textarea and minimal work on sending messages

--- cjquery_script.js
+++ cjquery_script.js
@@ -3,6 +3,31 @@ $(document).ready(
         var $userOptions = $("#user-options");
         var $userOptionsMenu = $userOptions.find("ul");
 
+        var $pendingMessage = $("#pending-message");
+
+        $pendingMessage.on("keydown", function (event) {
+            var keyID = event.keyCode;
+            switch (keyID) {
+                case 8:
+                    autoAdjustTextBox();
+                    break;
+                case 13:
+                    sendMessage();
+                    break;
+                case 46:
+                    autoAdjustTextBox();
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        if($pendingMessage.style.height >= 61) {
+            $pendingMessage.style.overflow = "scroll";
+        } else {
+            $pendingMessage.style.overflow = "hidden";
+        }
+
         $userOptions.find("input").click(
             function (event) {
                 $userOptionsMenu.toggle();
@@ -17,5 +42,22 @@ $(document).ready(
                 }
             }
         );
+
     }
 );
+
+function sendMessage() {
+
+    var pendingMessage = $("#pending-message").value;
+
+    $.ajax(
+        {
+            method: "POST",
+            url: "http://localhost:8080/",
+            data: {message: pendingMessage}
+        }
+    )
+        .done(function(response) {
+            console.log("Received response");
+        })
+}
