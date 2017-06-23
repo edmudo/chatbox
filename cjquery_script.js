9d5e4a7fa7ae54361584a9dd99ac20cf22d30948
Edmund Do <edo@uvm.edu>
2017-06-23 00:59:18 -0400

    Added functionality to parse JSON data from server
    Additionally, separated set up into several parts.

--- cjquery_script.js
+++ cjquery_script.js
@@ -1,37 +1,83 @@
 $(document).ready(
     function () {
+        setupPage();
 
-        setupUserOptions();
-
+        setupChatbox();
+        setupOptionsMenu();
     }
 );
 
 function setupPage() {
-
     // TODO: Get user id from successful login
 
     $.ajax(
         {
-            method: "POST",
+            method: "GET",                      // Temporary GET, switch to POST
             url: "http://localhost:8080/pull",
             xhrFields: {
                 withCredentials: true
             },
-            data: {user_id: 1}
+            data: {user_id: 3}
         }
     )
         .done(function(data, textStatus, xhr) {
             console.log(data);
-            console.log(textStatus);
-            console.log(xhr.statusText);
             console.log("Received response: " + xhr.statusText);
+            fillPage(data);
+            // TODO setupEventHandlers();
         });
 }
 
-function setupUserOptions() {
+function fillPage(chatProfile) {
+
+    for(var i = 0; i < chatProfile.threads.length; i++) {
+        var thread = chatProfile.threads[i];
+
+        // format data
+        var avatarURL = "https://qph.ec.quoracdn.net/main-thumb-422971-50-2rW6VfaPKyuCl1ZzHXrCcHwZu2z36PdT.jpeg",
+            tempStr = thread.thread_messages[0].message,
+            preview = tempStr.substr(0, 27) + "...",
+            currDate = new Date(),
+            date = new Date(thread.thread_messages[0].datetime_sent * 1000),
+            dateStr = "";
+
+        if(currDate.getFullYear() === date.getFullYear())
+            dateStr = (date.getMonth() + 1) + "/" + date.getDate();
+        else
+            dateStr = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+
+        var $chatTemplate = $($("#chat-template").html());
+        $chatTemplate.attr("data-thread-id", thread.thread_id);
+        $chatTemplate.find(".avatar img").attr("src", avatarURL);// link to submitted avatar
+        $chatTemplate.find(".chat-pane-name").html(thread.thread_name);
+        $chatTemplate.find(".chat-pane-preview-message").html(preview);
+        $chatTemplate.find(".chat-pane-preview-date").html(dateStr);
+
+        $("#chats").append($chatTemplate);
+    }
+}
+
+function setupOptionsMenu() {
     var $userOptions = $("#user-options");
     var $userOptionsMenu = $userOptions.find("ul");
 
+    $userOptions.find("input").click(
+        function (event) {
+            $userOptionsMenu.toggle();
+            event.stopPropagation();
+        }
+    );
+
+    $("body").click(
+        function () {
+            if($userOptionsMenu.css("display") !== "none") {
+                $userOptionsMenu.toggle();
+            }
+        }
+    );
+}
+
+function setupChatbox() {
     var $pendingMessage = $("#pending-message");
 
     $pendingMessage.on("keydown", function (event) {
@@ -50,21 +96,6 @@ function setupUserOptions() {
                 break;
         }
     });
-
-    $userOptions.find("input").click(
-        function (event) {
-            $userOptionsMenu.toggle();
-            event.stopPropagation();
-        }
-    );
-
-    $("body").click(
-        function () {
-            if($userOptionsMenu.css("display") !== "none") {
-                $userOptionsMenu.toggle();
-            }
-        }
-    );
 }
 
 function sendMessage() {
