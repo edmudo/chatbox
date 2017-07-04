f4c5dff5216fd5f100e2f415c9500ead64152926
Edmund Do <edo@uvm.edu>
2017-07-04 01:39:07 -0400

    Added optional fields
    Configuration to enable variations on handling the route. Callback is
    optional particularly in serving static pages that may want to utize
    the configurations. Also, added documentation.

--- Route.js
+++ Route.js
@@ -1,13 +1,42 @@
 module.exports = Route;
 
+const staticServer = require("./StaticHTTP");
+
 function Route() {
     this.pathHandlers = {};
 }
 
-Route.prototype.register = function(path, cb) {
+/**
+ * Register a defined route
+ *
+ * @param {String} path - pathname resembling "/example"
+ * @param {Object=} options - options for the callback
+ * @param {boolean} options.requireAuth - requires a session to continue
+ * @param {requestCallback=} cb - if specified, handle the response, otherwise attempt to serve file associated to path
+ */
+
+Route.prototype.register = function(path, options = {}, cb) {
+
+    if(typeof options === "function") {
+        cb = options;
+        options = {};
+    }
+
+    options = {
+        requireAuth: (typeof options.requireAuth !== "undefined") ? options.requireAuth : true
+    };
+
     if(typeof this.pathHandlers[path] === "undefined") {
-        this.pathHandlers[path] = cb;
-        console.log("Created route: " + path);
+        if(typeof cb !== "undefined")
+            this.pathHandlers[path] = cb;
+        else
+            this.pathHandlers[path] = function(req, res) {
+                staticServer.serveFile(req, res, path);
+            };
+
+        this.pathHandlers[path].requireAuth = options.requireAuth;
+
+        console.log(`Created route: ${path} with options: ${options.requireAuth}`);
     } else {
         console.log("Could not create route: " + path);
     }
